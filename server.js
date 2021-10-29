4bbe3211d9efa7ccae32dc8341f356ae11ef90f1
jeffhzak <jeffhzak@hotmail.com>
2021-10-29 17:33:27 +0800

    integrated express-session

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const session = require("express-session");
 
 const app = express();
 const port = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
+const SECRET = process.env.SECRET;
 
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
@@ -14,7 +16,11 @@ mongoose.connection.once("open", () => {
     console.log("connected to mongo");
 });
 
-
+app.use(session({
+    secret: SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
 
 //* MIDDLEWARE
 
