a5e853f4df92f1fab31fadc2c9b64fbc9b618693
jeffhzak <jeffhzak@hotmail.com>
2021-10-30 18:35:02 +0800

    list of all cars functional

--- client/src/cars/allcars.jsx
+++ client/src/cars/allcars.jsx
@@ -1,14 +1,58 @@
 import React from "react";
+import axios from "axios";
 import { NavLink } from "react-router-dom";
-import { useRef, useState } from "react";
+import { useState, useEffect } from "react";
+import { Box } from "@mui/system";
+import { Button, Card, CardActions, CardContent, CardMedia, Chip, LinearProgress, Typography } from "@mui/material";
+import { CarCard } from "../globalComponents/CarCard";
 
 function AllCars() {
+
+  const [fetchState, setFetchState] = useState("loading");
+  const [cars, setCars] = useState([]);
+  
+  useEffect(() => {
+    const fetchAllCars = async () => {
+      let isSubscribed = true;
+      const URL = `/api/cars/`;
+      try {
+        setFetchState("loading");
+        const data = await axios.get(URL);
+        console.log("data", data.data);
+        setFetchState("complete");
+        isSubscribed ? setCars(data.data) : console.log("fetch cancelled because of component unmount");
+      }
+      catch (error) {
+        setFetchState("error");
+        console.log("error situation");
+        history.replace("/");
+      }
+      return () => isSubscribed = false;
+    }
+    fetchAllCars();
+  }, []);
+  
+  const carsCardArrayRender = cars?.map((eachCar) => {
+    return (
+      <CarCard carInfo={eachCar} key={`cardCard${eachCar._id}`}/>
+    )
+  })
+
   return (
     <>
-      <h1>All car page (INDEX)</h1>
+      <Box sx={{width:"80%", margin:"auto"}}>
+      <h1>Allcars.jsx (INDEX)</h1>
       <NavLink to={"/"}>
-        <p>Back to Main Page</p>
+        <Typography variant="h5" >Back to Main Page</Typography>
       </NavLink>
+        <h1>List of All Cars:</h1>
+        <Box className="rowStyle">
+          {fetchState === "complete" ? carsCardArrayRender : <LinearProgress /> }
+          <h1>{JSON.stringify(cars)}</h1>
+        </Box>
+
+      
+      </Box>
     </>
   );
 }
