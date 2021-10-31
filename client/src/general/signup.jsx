a90561582dddafa03c17cfda0a9e6b950dcf7a9b
calvin <calvin_sim222@hotmail.com>
2021-10-31 19:58:04 +0800

    Done frontend login and sign up

--- client/src/general/signup.jsx
+++ client/src/general/signup.jsx
@@ -1,52 +1,162 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import { useRef, useState } from "react";
+import { NavLink, Redirect } from "react-router-dom";
+import { useState } from "react";
 
 function SignUp() {
-  const inputUserDetails = useRef();
-  const inputPasswordDetails = useRef();
-  const inputDisplayNameDetails = useRef();
-  const [user, setUser] = useState({
-    username: "",
-    password: "",
-    displayName: "",
-  });
+  const URL = "/api/users/new";
+
+  const [name, setName] = useState("");
+  const [inputpassword, setInputPassword] = useState("");
+  const [inputdisplayName, setInputDisplayName] = useState("");
+  const [status, setStatus] = useState("");
+
+  const typeName = (event) => {
+    setName(event.target.value);
+    console.log("name is this: ", event.target.value);
+  };
+  const typePassword = (event) => {
+    setInputPassword(event.target.value);
+  };
+  const typeDisplayName = (event) => {
+    setInputDisplayName(event.target.value);
+  };
+
+  const CreateUser = async () => {
+    const newUser = {
+      username: !!name ? name : null,
+
+      password: !!inputpassword ? inputpassword : null,
+      displayname: !!inputdisplayName ? inputdisplayName : null,
+    };
+    console.log(newUser);
+
+    // console.log("argument is this:", user);
+    // const res =  await axios.post(URL, newUser);
+    // res.data
+
+    const res = await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    });
+    const data = await res.json();
+    // addHolidayFn(data);
+    console.log("WE GOT THIS BACK FROM BACKEND", data);
+    setStatus(data);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     // const title = event.target.title.value;
     // console.log("title", title)
 
-    console.log(inputUserDetails.current.value);
-    console.log(inputPasswordDetails.current.value);
+    CreateUser();
   };
-  return (
-    <>
-      <h1>这个是 Sign Up Page!</h1>
-      <NavLink to={"/"}>
-        <p>Back to Main Page</p>
-      </NavLink>
-      <div className="login">
-        <form onSubmit={handleSubmit}>
-          <input
-            name="username"
-            placeholder="Desired username"
-            ref={inputUserDetails}
-          />
-          <input
-            name="password"
-            placeholder="Desired Password"
-            ref={inputPasswordDetails}
-          />
-          <input
-            name="displayname"
-            placeholder="Display Name"
-            ref={inputDisplayNameDetails}
-          />
-          <button>Create Account!</button>
-        </form>
-      </div>
-    </>
-  );
+  if (status === "Success") {
+    console.log("This is status: ", status);
+    return <Redirect to={"/login"} />;
+  } else {
+    return (
+      <>
+        <h1>这个是 Sign Up Page!</h1>
+        <NavLink to={"/"}>
+          <p>Back to Main Page</p>
+        </NavLink>
+        <div className="login">
+          <form onSubmit={handleSubmit}>
+            <input
+              name="username"
+              placeholder="Desired username"
+              onChange={typeName}
+            />
+            <input
+              name="password"
+              placeholder="Desired Password"
+              onChange={typePassword}
+            />
+            <input
+              name="displayname"
+              placeholder="Display Name"
+              onChange={typeDisplayName}
+            />
+            <button>Create Account!</button>
+          </form>
+        </div>
+      </>
+    );
+  }
 }
 
 export default SignUp;
+
+// function SignUp() {
+//   const URL = "/api/users/new";
+//   const inputUserDetails = useRef();
+//   const inputPasswordDetails = useRef();
+//   const inputDisplayNameDetails = useRef();
+//   const [user, setUser] = useState({
+//     username: "",
+//     password: "",
+//     displayName: "",
+//   });
+
+//   const CreateUser = async (title) => {
+//     console.log("argument is this:", title);
+
+//     const res = await fetch(URL, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(title),
+//     });
+//     const data = await res.json();
+//     // addHolidayFn(data);
+//     console.log("WE GOT THIS BACK FROM BACKEND", data);
+//   };
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     // const title = event.target.title.value;
+//     // console.log("title", title)
+
+//     setUser({
+//       username: inputUserDetails.current.value,
+//       password: inputPasswordDetails.current.value,
+//       displayname: inputDisplayNameDetails.current.value,
+//     });
+//     CreateUser(user);
+//   };
+//   return (
+//     <>
+//       <h1>这个是 Sign Up Page!</h1>
+//       <NavLink to={"/"}>
+//         <p>Back to Main Page</p>
+//       </NavLink>
+//       <div className="login">
+//         <form onSubmit={handleSubmit}>
+//           <input
+//             name="username"
+//             placeholder="Desired username"
+//             ref={inputUserDetails}
+//           />
+//           <input
+//             name="password"
+//             placeholder="Desired Password"
+//             ref={inputPasswordDetails}
+//           />
+//           <input
+//             name="displayname"
+//             placeholder="Display Name"
+//             ref={inputDisplayNameDetails}
+//           />
+//           <button>Create Account!</button>
+//         </form>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default SignUp;
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,52 +7,54 @@ const bcrypt = require("bcrypt");
 //! SEED
 
 router.get("/seed", async (req, res) => {
-    await Users.deleteMany({});
-    // console.log(seedUsers);
-    
-    seedUsers.forEach((user) => {
-        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10));
-    });
+  await Users.deleteMany({});
+  // console.log(seedUsers);
 
-    const seededUsers = await Users.create(seedUsers);
-    
-    res.json(seededUsers);
-})
+  seedUsers.forEach((user) => {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10));
+  });
+
+  const seededUsers = await Users.create(seedUsers);
+
+  res.json(seededUsers);
+});
 
 //! INDEX
 
 router.get("/", async (req, res) => {
-    const allUsers = await Users.find({});
+  const allUsers = await Users.find({});
 
-    res.json(allUsers);
-})
+  res.json(allUsers);
+});
 
 //! SHOW
 
 router.get("/:id", async (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    const thisUser = await Users.findById(id);
+  const thisUser = await Users.findById(id);
 
-    res.json(thisUser);
-})
+  res.json(thisUser);
+});
 
 //! CREATE
 
 router.post("/new", async (req, res) => {
-    const newUserData = req.body;
-    newUserData.password = bcrypt.hashSync(newUserData.password, bcrypt.genSaltSync(10));
+  const newUserData = req.body;
+  newUserData.password = bcrypt.hashSync(
+    newUserData.password,
+    bcrypt.genSaltSync(10)
+  );
 
-    const newUser = await Users.create(newUserData);
+  const newUser = await Users.create(newUserData);
 
-    res.json(newUser);
-})
+  // res.json(newUser);
+  res.json("Success");
+});
 
 //! EDIT
 
-
 //! UPDATE
-
 
 //! DESTROY
 
