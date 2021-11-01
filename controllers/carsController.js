f613a6f9d7600ca3be092beafa9075c4ef1db1c1
jeffhzak <jeffhzak@hotmail.com>
2021-11-01 11:50:18 +0800

    fix merging issues

--- controllers/carsController.js
+++ controllers/carsController.js
@@ -23,17 +23,43 @@ router.get("/", async (req, res) => {
 
 
 //! SHOW
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    const thisCar = await Cars.findById(id);
+    res.json(thisCar);
+  })
 
 
-//! CREATE
+//! CREATE not working
+router.post("/new", async (req, res) => {
+    console.log("data", req.body);
+    const cars = await Cars.create(req.body);
+    res.json(cars);
+  });
 
 
 //! EDIT
+//React route to edit page
 
 
 //! UPDATE
+router.put("/:id/edit", async (req, res) => {
+    const { id } = req.params; 
+    const car = await Cars.findByIdAndUpdate(id, req.body)
+    res.json(car)
+  })
 
 
 //! DESTROY
+router.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+      const result = await Cars.findByIdAndDelete(id);
+      res.json(result);
+    } catch (error) {
+      res.json({ error });
+    }
+  });
+  
 
 module.exports = router;
