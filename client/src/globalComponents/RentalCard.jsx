43f6eba3540ef1fb115e4248b22c2345b7e551e4
jeffhzak <jeffhzak@hotmail.com>
2021-11-04 13:01:41 +0800

    finalized functionality of notif page, about to add date-fns

--- client/src/globalComponents/RentalCard.jsx
+++ client/src/globalComponents/RentalCard.jsx
@@ -4,6 +4,7 @@ import { Box } from '@mui/system';
 import {useContext} from "react";
 import { DataContext } from "../App";
 import axios from "axios"
+import { Link } from 'react-router-dom';
 
 
 export const RentalCard = ({rentalEvent, rentalEvents, setRentalEvents}) => {
@@ -11,6 +12,7 @@ export const RentalCard = ({rentalEvent, rentalEvents, setRentalEvents}) => {
     const { user, setUser } = useContext(DataContext);
     const carInfo = rentalEvent.car_rented;
     const carOwner = rentalEvent?.original_owner?._id;
+    const carOwnerAvatar = rentalEvent?.original_owner?.display_picture;
     const thisIsMyCar = carOwner === user._id;
     const confirmedRental = rentalEvent.owner_confirmation;
 
@@ -19,16 +21,25 @@ export const RentalCard = ({rentalEvent, rentalEvents, setRentalEvents}) => {
         const res = await axios.delete(URL);
         const data = res.data;
         console.log(data);
-        // console.log(rentalEvents);
+        console.log(rentalEvents);
         const newRentalEvents = rentalEvents.filter(arrayStep => arrayStep._id !== rentalEvent._id);
         // console.log(newRentalEvents);
         setRentalEvents(newRentalEvents);
     }
 
     const handleConfirmEvent = async () => {
+        const URL = `/api/carRentalEvents/${rentalEvent._id}`
+        const res = await axios.put(URL, {...rentalEvent, owner_confirmation: true});
+        const data = res.data;
+        console.log("data", data);
+
+
         const foundIndex = rentalEvents.findIndex(arrayStep => arrayStep._id === rentalEvent._id);
         console.log(foundIndex);
-        
+        const dupeRentalEvents = [...rentalEvents];
+        dupeRentalEvents[foundIndex].owner_confirmation = true;
+        console.log(dupeRentalEvents);
+        setRentalEvents(dupeRentalEvents);
     }
 
     const unconfirmedButtons = [
@@ -69,18 +80,27 @@ export const RentalCard = ({rentalEvent, rentalEvents, setRentalEvents}) => {
     return (
         <Box className="colStyle" sx={thisIsMyCar ? {backgroundColor: "lightgreen", ml:"0.5em", mr:"0.5em"} : {ml:"0.5em", mr:"0.5em"}}>
         <Card sx={{ width: "20em", m:"0.5em"}} key={`card-${carInfo?._id}`}>
-            <CardMedia
-            component="img"
-            height="140px"
-            image={carInfo?.images[0]}
-            alt="Picture of the damn car"
-            />
+            <Box sx={{position: "relative"}}>
+                <Link to={`/cars/${carInfo._id}`}>
+                <CardMedia
+                component="img"
+                height="140px"
+                image={carInfo?.images[0]}
+                sx={{position: "relative"}}
+                alt="Picture of the damn car"
+                />
+                </Link>
+                <Link to={`/users/${carOwner}`}>
+                 <img className="user_avatar_floating" src={`${carOwnerAvatar}`} />
+                </Link>
+            </Box>
             <CardContent>
             <Typography gutterBottom variant="h5" component="div">
                 {carInfo?.brand} {carInfo?.model}
             </Typography>
             <Box className="colStyle">
-                <Button onClick={()=>console.log(rentalEvent)}>log</Button>
+                {/* <Button onClick={()=>console.log(rentalEvent)}>log</Button>
+                <Button onClick={()=>console.log(rentalEvents)}>rentalEvents</Button> */}
             <Typography gutterBottom variant="h7">
                 {confirmedRental ? "Loan Duration:" : "Proposed Rent Date:"}
             </Typography>
--- client/src/users/Notifications.jsx
+++ client/src/users/Notifications.jsx
@@ -51,6 +51,7 @@ export const Notifications = () => {
     return (
         <>
         <h1>Notifications.jsx</h1>
+        <button onClick={() => console.log(rentalEvents)}>rentalEvents</button>
         <h2>Loans Awaiting Confirmation:</h2>
         {fetchState === "complete" ? 
         <>
@@ -71,7 +72,7 @@ export const Notifications = () => {
                 const carOwner = thisREvent?.original_owner?._id;
                 const thisIsMyCar = carOwner === user._id;
                 if (thisREvent.owner_confirmation === true) return (
-                    <RentalCard key={`${user._id}keyloan${thisREvent._id}`} rentalEvent={thisREvent} setRentalEvents={setRentalEvents}/>
+                    <RentalCard key={`${user._id}keyloan${thisREvent._id}`} rentalEvent={thisREvent} setRentalEvents={setRentalEvents} rentalEvents={rentalEvents}/>
                 )
             })}
         </Box>
